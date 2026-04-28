95b9503a5567895fb7242f9d4a630a8e5294961c
RuariW12 <ruariw16@gmail.com>
2026-04-28 11:24:54 -0400

    configured oidc vars

---
+++ infrastructure/oidc/variables.tf
@@ -0,0 +1,27 @@
+variable "project_name" {
+  description = "Project name"
+  type        = string
+  default     = "maroon-ledger"
+}
+
+variable "region" {
+  description = "AWS region"
+  type        = string
+  default     = "us-east-2"
+}
+
+variable "account_id" {
+  description = "AWS account ID"
+  type        = string
+}
+
+variable "github_org" {
+  description = "GitHub organization or username"
+  type        = string
+  default     = "RuariW12"
+}
+
+variable "github_repo" {
+  description = "GitHub repository name for the pipeline"
+  type        = string
+}
