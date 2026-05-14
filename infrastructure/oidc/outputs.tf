70acb6c1d90df9fd11de8b4a4cf6895cec3e2f8f
RuariW12 <ruariw16@gmail.com>
2026-05-14 12:50:54 -0400

    fixed bug in output and added network outputs

--- infrastructure/oidc/outputs.tf
+++ infrastructure/oidc/outputs.tf
@@ -6,3 +6,19 @@ output "role_arn" {
 output "ecr_repository_url" {
   description = "ECR repository URL for container image pushes"
   value       = aws_ecr_repository.app.repository_url
+}
+
+output "vpc_id" {
+  description = "VPC ID"
+  value       = aws_vpc.main.id
+}
+
+output "public_subnet_ids" {
+  description = "Public subnet IDs"
+  value       = aws_subnet.public[*].id
+}
+
+output "private_subnet_ids" {
+  description = "Private subnet IDs"
+  value       = aws_subnet.private[*].id
+}
