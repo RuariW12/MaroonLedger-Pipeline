5465776ae305cf7f71aeb91d4c3f68417ab8a062
RuariW12 <ruariw16@gmail.com>
2026-05-14 12:54:23 -0400

    added app port to outputs

--- infrastructure/oidc/variables.tf
+++ infrastructure/oidc/variables.tf
@@ -31,3 +31,9 @@ variable "vpc_cidr" {
   type        = string
   default     = "10.0.0.0/16"
 }
+
+variable "app_port" {
+  description = "Port the application container listens on"
+  type        = number
+  default     = 8000
+}
